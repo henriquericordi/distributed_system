62363e162743a9280d2d5f135d3d5b81e3b07f38
José Henrique Ricordi Cruz <cc45966446830@L4C17X.lab.cct.uenp.edu.br>
2019-06-12 17:32:37 -0300

    Add REST

---
+++ Rest/src/java/ws/Aluno.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+/**
+ *
+ * @author cc45966446830
+ */
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }   
+}
